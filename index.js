d7c325d0d3f4ae9ef3143bf0212a357824aec7cb
0m3ga13 <barkaoussama13@gmail.com>
2023-07-21 00:27:46 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+
+ const distanceFromHqInBlocks = (numBlocks) => Math.abs(42-numBlocks)  ;
+ const distanceFromHqInFeet = (numBlocks) => Math.abs(42-numBlocks)*264;
+ const distanceTravelledInFeet = (block1, block2) => Math.abs(block2-block1) * 264;
+ const calculatesFarePrice = (start, destination) =>
+      distanceTravelledInFeet(start, destination) < 400 ? 0
+    : distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000 ? (distanceTravelledInFeet(start, destination)-400)*0.02
+    : distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500 ? 25
+    : 'cannot travel that far';
